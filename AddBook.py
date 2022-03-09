68cc05da9a691e83d22e18fbd051f18b4e9c1686
Kevin Ng'ang'a <knganga28@gmail.com>
2022-03-09 02:12:30 -0800

    add book function

--- AddBook.py
+++ AddBook.py
@@ -4,7 +4,9 @@ from tkinter import messagebox
 import pymysql
 
 def bookRegister():
-
+    """function executes an SQL command to insert data into the table
+       and commit the changes.
+    """
     bid = bookInfo1.get()
     title = bookInfo2.get()
     author = bookInfo3.get()
@@ -26,29 +28,36 @@ def bookRegister():
     root.destroy()
 
 def addBook():
-
+    """function connects to the MySql server and creates a window for
+       new text fields that collects information from user and and then calls
+       bookRegister() function to add books to the table.
+    """
     global bookInfo1, bookInfo2, bookInfo3, bookInfo4, Canvas1, con, cur, bookTable, root
 
-    root = Tk()
-    root.title("Library")
-    root.minsize(width=400,height=400)
-    root.geometry("600x500")
-    mypass = "root"
-    mydatabase="db"
-    con = pymysql.connect( host="localhost",user="root",password=mypass,database=mydatabase)
+    add_book_screen = Tk()
+    add_book_screen.title("Library")
+    add_book_screen.minsize(width=400,height=400)
+    add_book_screen.geometry("600x500")
+
+    mypass = ""
+    database_name =""
+    con = pymysql.connect(host="",user="",password=mypass,database=database_name)
     cur = con.cursor()
-    # Enter Table Names here
-    bookTable = "books" # Book Table
-    Canvas1 = Canvas(root)
+
+    # Table Name
+    bookTable = ""
+    Canvas1 = Canvas(add_book_screen)
     
     Canvas1.config(bg="#ff6e40")
     Canvas1.pack(expand=True,fill=BOTH)
     
-    headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
+    headingFrame1 = Frame(add_book_screen,bg="#FFBB00",bd=5)
     headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
+
     headingLabel = Label(headingFrame1, text="Add Books", bg='black', fg='white', font=('Courier',15))
     headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
-    labelFrame = Frame(root,bg='black')
+
+    labelFrame = Frame(add_book_screen,bg='black')
     labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4)
     
     # Book ID
@@ -80,10 +89,10 @@ def addBook():
     bookInfo4.place(relx=0.3,rely=0.65, relwidth=0.62, relheight=0.08)
     
     #Submit Button
-    SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black', command=bookRegister)
+    SubmitBtn = Button(add_book_screen,text="SUBMIT",bg='#d1ccc0', fg='black', command=bookRegister)
     SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
     
-    quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
+    quitBtn = Button(add_book_screen,text="Quit",bg='#f7f1e3', fg='black', command=add_book_screen.destroy)
     quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
     
-    root.mainloop()
+    add_book_screen.mainloop()
